2aa3b93c682d93126de2f6fe48beda98e0048e93
karl3009 <karlp2000@gmail.com>
2023-10-29 19:07:12 +0100

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,39 +1,38 @@
 public class GameOfLife {
-    //felter 
+    // felter
     private int[][] matrix;
     private int Size;
-    
-    //konstruktører
+
+    // konstruktører
     public GameOfLife(int n) {
         this.Size = n;
         initGame();
     }
 
-    /*
-     * public GameOfLife(int[][] initialState) {
-     * int is1 = Random.c(1,0);
-     * int is2 = Random.c(1,0);
-     * int is3 = Random.c(1,0);
-     * int is4 = Random.c(1,0);
-     * int is5 = Random.c(1,0);
-     * int is6 = Random.c(1,0);
-     * int is7 = Random.c(1,0);
-     * int is8 = Random.c(1,0);
-     * int is9 = Random.c(1,0);
-     * initialState = int matrix[]={{is1,is2,is3},{is4,is5,is6},{is7,is8,is9}};
-     * }
-     */
+    public GameOfLife(int[][] initialState) {
+        int is1 = Random.c(1,0);
+        int is2 = Random.c(1,0);
+        int is3 = Random.c(1,0);
+        int is4 = Random.c(1,0);
+        int is5 = Random.c(1,0);
+        int is6 = Random.c(1,0);
+        int is7 = Random.c(1,0);
+        int is8 = Random.c(1,0);
+        int is9 = Random.c(1,0);
+        this.matrix[0][0]={{is1,is2,is3},{is4,is5,is6},{is7,is8,is9}};
+    }
 
-    public void initGame(){
+    public void initGame() {
         this.matrix = new int[Size][Size];
         for (int i = 0; i < 3; i++) {
-            for(int j = 0; j<3; j++){
+            for (int j = 0; j < 3; j++) {
                 matrix[i][j] = j;
             }
         }
-        
+
     }
-    //instants-metoder    
+
+    // instants-metoder
     public int liveNeighbours(int x, int y) {
         return matrix[x][y];
         /*
@@ -43,7 +42,8 @@ public class GameOfLife {
          * }
          */
     }
-    public void nextState(){
+
+    public void nextState() {
 
     }
 
