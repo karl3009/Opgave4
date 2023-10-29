9e1da4ab9fd78d158fb2b327d9c040c3f2c5fb5f
karl3009 <karlp2000@gmail.com>
2023-10-29 17:58:13 +0100

    Update GameOfLife.java

---
+++ GameOfLife.java
@@ -0,0 +1,11 @@
+public class GameOfLife {
+    public GameOfLife(int n){
+        nextState();
+        liveNeighbours(int x, int y);
+
+    }
+    public GameOfLife(int [] [] initialState){
+        
+    }
+
+}
