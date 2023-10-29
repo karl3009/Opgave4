d2a00748fdd8143011738e30b6e58a0650552f96
karl3009 <karlp2000@gmail.com>
2023-10-29 23:49:39 +0100

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,9 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class GameOfLife {
-    // felter
+
     private int[][] matrix;
     private int Size;
 
-    // konstruktører
     public GameOfLife(int n) {
         this.Size = n;
         initGame();
@@ -37,7 +40,6 @@ public class GameOfLife {
         }
     }
 
-    // instants-metoder
     public int AliveNeighbours(int x, int y) {
         int counter = 0;
         for (int i = -1; i < 2; i++) {
@@ -171,34 +173,59 @@ public class GameOfLife {
         return counter;
     }
 
-    public void nextState() {
-        System.out.println(matrix[2][0] == 1 && AliveNeighbours(3, 1)>3);
+    public void nextState() { // virker ikke ordenligt.
         for (int i = 0; i < Size - 1; i++) {
             for (int j = 0; j < Size - 1; j++) {
                 if (matrix[i][j] == 1 && AliveNeighbours(i, j) < 2) {
                     matrix[i][j] = 0;
-                    System.out.println('x');
                 } else if (matrix[i][j] == 1 && AliveNeighbours(i, j) > 3) {
                     matrix[i][j] = 0;
-                    System.out.println('z');
-                }
-                else if (matrix[i][j] == 0 && AliveNeighbours(i, j) == 3) {
+                } else if (matrix[i][j] == 0 && AliveNeighbours(i, j) == 3) {
                     matrix[i][j] = 1;
                 } else {
                     matrix[i][j] = 1;
                 }
             }
         }
-        // printMatrix(matrix);
         drawMatrix(matrix);
     }
 
-    public void printMatrix(int[][] m) {
-        for (int i = 0; i < Size; i++) {
-            for (int j = 0; j < Size; j++) {
-                System.out.print(m[i][j]);
+    public void drawFiletrix(int[][] m) {
+
+        try {
+            int rows = 0;
+            int cols = 0;
+            readFile();
+
+            for (int i = 0; i <= rows; i++) {
+                for (int j = 0; j <= cols; j++) {
+                    if (matrix[rows][cols] == 1) {
+                        StdDraw.point(i, j);
+                    }
+                }
             }
-            System.out.println("");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            for (int i = 0; i <= Size; i++) {
+                for (int j = 0; j <= Size; j++) {
+                    if (matrix[i][j] == 1) {
+                        StdDraw.point(i, j);
+                    }
+                }
+            }
+        }
+
+    }
+        public void readFile() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("./gol/toad.gol"));
+        String n = input.nextLine();
+        int rows = n.length();
+        int cols = 0;
+        while (input.hasNextInt()==false){
+            cols += 1;
+        }
+        if (input.nextInt()==1){
+            matrix[rows][cols]=1;
         }
     }
 }
