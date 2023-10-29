74d64077d8bd68cd3ec1e97b021c388a24c6f6ba
GamerCoke <35891558+GamerCoke@users.noreply.github.com>
2023-10-29 21:23:50 +0100

    Update GameOfLifeMain.java
    change int n for GameOfLife variable

--- GameOfLifeMain.java
+++ GameOfLifeMain.java
@@ -1,7 +1,8 @@
 public class GameOfLifeMain {
     public static void main(String[] args) {
-        GameOfLife game = new GameOfLife(3);
-        System.out.println(game.liveNeighbours(2, 2));
-        //StdDraw.setCanvasSize(1000, 1000);
+        int n = 4;
+        GameOfLife game = new GameOfLife(n);
+        System.out.println(game.AliveNeighbours(2, 2));
     }
+
 }
