c8658d28e4b3e3c4cdef1d0873fdfc903df233d7
karl3009 <karlp2000@gmail.com>
2023-10-29 23:34:11 +0100

    asd

--- GameOfLife.java
+++ GameOfLife.java
@@ -9,46 +9,33 @@ public class GameOfLife {
         initGame();
     }
 
-    /*
-     * public GameOfLife(int[][] initialState) {
-     * Random rand = new Random();
-     * for (int i = 0; i < 3; i++) {
-     * for (int j = 0; j < 3; j++) {
-     * matrix[i][j] = rand.nextInt(1);
-     * }
-     * }
-     * }
-     */
-
     public void initGame() {
+        double scale = Size + Size / 4;
+        StdDraw.setXscale(-scale, scale);
+        StdDraw.setYscale(-scale, scale);
+        StdDraw.setPenRadius(0.5 / scale);
+        StdDraw.setPenColor(StdDraw.BLACK);
         this.matrix = new int[Size][Size];
         for (int i = 0; i < Size; i++) {
             for (int j = 0; j < Size; j++) {
                 matrix[i][j] = 1;
-                System.out.print(matrix[i][j]);
+            }
+        }
+        drawMatrix(matrix);
+    }
+
+    public void drawMatrix(int[][] m) {
+        StdDraw.clear();
+        for (int i = 0; i < Size; i++) {
+            for (int j = 0; j < Size; j++) {
+                System.out.print(m[i][j]);
+                if (m[j][i] == 1) {
+                    StdDraw.point(i, -j);
+                }
             }
             System.out.println("");
         }
-
     }
-
-    public void drawBoard(String[][] board){
-        for (int i = 0; i < 3; i++) { //line 88
-            for (int j = 0; j < 3; j++) {
-                int r = board.length;
-                int c = board[0].length;
-                double R = new Double(r);
-                double C = new Double(c);
-    
-            StdDraw.setXscale(0,C);
-            StdDraw.setYscale(0,R);
-            StdDraw.setCanvasSize(500,500);
-            StdDraw.setPenRadius(0.05);
-            StdDraw.setPenColor(StdDraw.BLUE);
-            StdDraw.rectangle((C/2), (R/2), (C/2), (R/2));
-            StdDraw.close();
-            }
-        }
 
     // instants-metoder
     public int AliveNeighbours(int x, int y) {
@@ -57,111 +44,153 @@ public class GameOfLife {
             if (x == 0 && y == 0) {
                 for (int j = 0; j < 2; j++) {
                     if (matrix[x][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x + 1][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                 }
-            } else if (x == Size && y == 0) {
+                if (matrix[x][y] == 1) {
+                    counter -= 1;
+                }
+                break;
+            } else if (x == Size - 1 && y == 0) {
                 for (int j = 0; j < 2; j++) {
                     if (matrix[x - 1][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
+
                 }
-            } else if (x == 0 && y == Size) {
-                for (int j = 0; j < 2; j++) {
+                if (matrix[x - 1][y] == 1) {
+                    counter -= 1;
+                }
+                break;
+            } else if (x == 0 && y == Size - 1) {
+                for (int j = 1; j <= 2; j++) {
                     if (matrix[x][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x + 1][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                 }
-            } else if (x == Size && y == Size) {
+                if (matrix[x][y - 1] == 1) {
+                    counter -= 1;
+                }
+                break;
+            } else if (x == Size - 1 && y == Size - 1) {
                 for (int j = 0; j < 2; j++) {
                     if (matrix[x][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x - 1][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                 }
-            }
-
-            else if (x == 0) {
+                if (matrix[x - 1][y - 1] == 1) {
+                    counter -= 1;
+                }
+                break;
+            } else if (x == 0) {
                 if (matrix[x][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
                 }
                 if (matrix[x + 1][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
+                }
+                if (i == 1) {
+                    if (matrix[x][y] == 1) {
+                        counter -= 1;
+                    }
                 }
             } else if (y == 0) {
                 for (int j = 0; j < 2; j++) {
                     if (matrix[x + 1][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x - 1][y + j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                 }
-            } else if (x == Size) {
+                if (matrix[x][y] == 1) {
+                    counter -= 1;
+                }
+                break;
+            } else if (x == Size - 1) {
                 if (matrix[x][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
                 }
                 if (matrix[x - 1][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
                 }
-            } else if (y == Size) {
+                if (i == 1) {
+                    if (matrix[x][y] == 1) {
+                        counter -= 1;
+                    }
+                }
+            } else if (y == Size - 1) {
                 for (int j = 0; j < 2; j++) {
                     if (matrix[x + 1][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                     if (matrix[x - 1][y - j] == 1) {
-                        counter = +1;
+                        counter += 1;
                     }
                 }
+                if (matrix[x][y - 1] == 1) {
+                    counter -= 1;
+                }
+                break;
             } else {
                 if (matrix[x + 1][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
                 }
                 if (matrix[x][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
                 }
                 if (matrix[x - 1][y + i] == 1) {
-                    counter = +1;
+                    counter += 1;
+                }
+                if (i == 1) {
+                    if (matrix[x][y] == 1) {
+                        counter -= 1;
+                    }
                 }
             }
 
-        }
-        if (matrix[x][y] == 1) {
-            counter -= 1;
         }
         return counter;
     }
 
     public void nextState() {
-        for (int i = 0; i < Size-1; i++) {
-            for (int j = 0; j < Size-1; j++) {
-                if (matrix[i][j] == 1 && (AliveNeighbours(i, j) < 2 || AliveNeighbours(i, j) > 3)) {
+        System.out.println(matrix[2][0] == 1 && AliveNeighbours(3, 1)>3);
+        for (int i = 0; i < Size - 1; i++) {
+            for (int j = 0; j < Size - 1; j++) {
+                if (matrix[i][j] == 1 && AliveNeighbours(i, j) < 2) {
                     matrix[i][j] = 0;
-                } else if (matrix[i][j] == 0 && AliveNeighbours(i, j) == 3) {
+                    System.out.println('x');
+                } else if (matrix[i][j] == 1 && AliveNeighbours(i, j) > 3) {
+                    matrix[i][j] = 0;
+                    System.out.println('z');
+                }
+                else if (matrix[i][j] == 0 && AliveNeighbours(i, j) == 3) {
                     matrix[i][j] = 1;
                 } else {
                     matrix[i][j] = 1;
                 }
             }
         }
-        printMatrix(matrix);
+        // printMatrix(matrix);
+        drawMatrix(matrix);
     }
 
     public void printMatrix(int[][] m) {
@@ -172,22 +201,4 @@ public class GameOfLife {
             System.out.println("");
         }
     }
-
-    /*
-     * public void nextState() {
-     * int cell = n;
-     * if (cell==1){
-     * if (nCount==1){
-     * cell = 0;
-     * } if (nCount>=4){
-     * cell = 0;
-     * }
-     * }elif (cell==1){
-     * if (nCount==3){
-     * cell = 1;
-     * }
-     * }
-     * }
-     */
-
 }
--- GameOfLifeMain.java
+++ GameOfLifeMain.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class GameOfLifeMain {
     public static void main(String[] args) {
-        GameOfLife game = new GameOfLife(4);
-        System.out.println(game.AliveNeighbours(1, 1));
-        System.out.println("aa");
-        //StdDraw.setCanvasSize(1000, 1000);
-        game.nextState();
-        game.nextState();
-        game.nextState();
+        Scanner input = new Scanner(System.in);
+        int n = 4;
+        GameOfLife game = new GameOfLife(n);
+        System.out.println(game.AliveNeighbours(2, 0));
+        StdDraw.show();
+        while (true) {
+            int in = checkInput(input);
+            if(in == 1){
+                game.nextState();
+            }
+            else{
+                System.exit(0);
+            }
+        }
+        
+    }
+        public static int checkInput(Scanner input) {
+        if (!input.hasNextInt()) {
+            throw new IllegalArgumentException("Input is not an integer ");
+        }
+        int verifiedInput = input.nextInt();
+        if (verifiedInput < 1) {
+            System.out.println("Input is under 1");
+            return 0;
+        }
+        else {
+            return verifiedInput;
+        }
     }
 }
