77ead2bc6a9a7ea24e18b7870a08efddefba6985
karl3009 <karlp2000@gmail.com>
2023-10-29 17:58:52 +0100

    Update GameOfLifeMain.java

--- GameOfLifeMain.java
+++ GameOfLifeMain.java
@@ -1,9 +1,7 @@
- public class GameOfLifeMain {
-    public static void main(String[]args){
-    StdDraw.setCanvasSize(1000,1000);
-    StdDraw.show(n);
-    StdDraw.Clear();
+public class GameOfLifeMain {
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(1000, 1000);
+        StdDraw.show(n);
+        StdDraw.Clear();
     }
-    
-
 }
